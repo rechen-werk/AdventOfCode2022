ffc4bb2794da0977072feebbf2e607329d9878fd
Adrian Vinojčić <k11904250@students.jku.at>
2022-12-04 00:10:57 +0100

    3. December

---
+++ src/december/December3.kt
@@ -0,0 +1,28 @@
+package december
+
+import util.Input
+
+class December3 : December<Int, Int> {
+    override fun star1(): Int = Input
+        .lines(3)
+        .sumOf {
+        mapPriority(it
+            .substring(0, it.length/2).toSet()
+            .intersect(it.substring(it.length/2).toSet())
+            .first()
+        )
+    }
+
+    override fun star2(): Int = Input
+        .lines(3)
+        .map { it.toSet() }
+        .chunked(3)
+        .sumOf {
+            mapPriority(it[0].intersect(it[1].intersect(it[2])).first())
+        }
+
+    private fun mapPriority(common: Char): Int =
+        if (common in 'a'..'z') common - 'a' + 1
+        else common - 'A' + 27
+
+}
