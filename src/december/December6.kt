dea77ae3c911bfe4afde4f42d988d52cde8bcda3
Adrian Vinojcic <adrian.vinojcic@gmail.com>
2022-12-06 16:09:54 +0100

    toSet() instead ot toList().size

--- src/december/December6.kt
+++ src/december/December6.kt
@@ -10,9 +10,9 @@ class December6 : December<Int, Int> {
 
     override fun star1(): Int = WINDOW_1 + Input.get(6)
         .windowed(WINDOW_1,1)
-        .indexOfFirst { it.toList().distinct().size == WINDOW_1 }
+        .indexOfFirst { it.toSet().size == WINDOW_1 }
 
     override fun star2(): Int = WINDOW_2 + Input.get(6)
         .windowed(WINDOW_2,1)
-        .indexOfFirst { it.toList().distinct().size == WINDOW_2 }
+        .indexOfFirst { it.toSet().size == WINDOW_2 }
 }
