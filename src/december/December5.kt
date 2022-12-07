0677300cfeedec61f9c9876f6fc4b5b62fcdd96c
rechenwerk-a <adrian.vinojcic@gmail.com>
2022-12-07 01:43:17 +0100

    5. December improvements

--- src/december/December5.kt
+++ src/december/December5.kt
@@ -7,53 +7,52 @@ class December5 : December<String, String> {
     override fun star1(): String {
         val (stacks, instructions) = input()
         instructions.forEach {
-            stacks[it["to"]]?.addAll(stacks[it["from"]]?.takeLast(it["move"]!!)?.reversed() ?: throw IllegalStateException())
-            for (i in 0 until it["move"]!!) {
-                stacks[it["from"]]?.removeLast()
+            stacks[it[2]-1].addAll(stacks[it[1]-1].takeLast(it[0]).reversed())
+            for (i in 0 until it[0]) {
+                stacks[it[1]-1].removeLast()
             }
         }
 
-        return stacks.map { it.value.last() }.joinToString("")
+        return stacks.map { it.last() }.joinToString("")
     }
 
     override fun star2(): String {
         val (stacks, instructions) = input()
         instructions.forEach {
-            stacks[it["to"]]?.addAll(stacks[it["from"]]?.takeLast(it["move"]!!) ?: throw IllegalStateException())
-            for (i in 0 until it["move"]!!) {
-                stacks[it["from"]]?.removeLast()
+            stacks[it[2]-1].addAll(stacks[it[1]-1].takeLast(it[0]))
+            for (i in 0 until it[0]) {
+                stacks[it[1]-1].removeLast()
             }
         }
-        return stacks.map { it.value.last() }.joinToString("")
+        return stacks.map { it.last() }.joinToString("")
     }
 
-    private fun input(): Pair<Map<Int, ArrayList<Char>>, List<Map<String, Int>>> = Input
-        .get(5)
-        .split("\n\n")
-        .chunked(2) { it[0]
+    private fun input(): Pair<List<ArrayList<Char>>, List<List<Int>>> {
+        val input = Input.get(5).split("\n\n")
+        return input[0]
             .split("\n")
             .dropLast(1)
             .reversed()
-            .map { it
-                .filterIndexed { i, _ -> (i % 4) == 1 }
-                .withIndex()
-                .groupBy { it.index }
-                .flatMap { it.value }
-            }
-            .flatten()
-            .groupBy({ it.index }, { it.value })
-            .map { it
-                .value
-                .dropLastWhile() { it.isWhitespace() } as ArrayList<Char>
-            }.mapIndexed {i, v -> i+1 to v}
-            .toMap() to it[1]
-            .split("\n")
-            .dropLast(1)
-            .map { it
-                .split(" ")
-                .chunked(2) { it[0] to it[1].toInt() }
-                .toMap()
+            .map { it.toList().filterIndexed { i, _ -> (i % 4) == 1 } }
+            .transpose()
+            .map { it.dropLastWhile { it.isWhitespace() } as ArrayList } to
+                input[1]
+                    .split("\n")
+                    .dropLast(1)
+                    .map { it
+                        .split("move ", " from ", " to ")
+                        .drop(1)
+                        .map { it.toInt() }
+                    }
+    }
+
+    private fun <T> List<List<T>>.transpose(): List<List<T>> {
+        val cols = this[0].size
+        val rows = this.size
+        return List(cols) { j ->
+            List(rows) { i ->
+                this[i][j]
             }
         }
-        .first()
+    }
 }
